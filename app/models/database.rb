42e299bf42455df077d1c0c0f914298dd96e9ffb
EC2 Default User <ec2-user@ip-172-31-24-191.ec2.internal>
2019-01-07 01:43:31 +0000

    Added exercises in database file

--- app/models/database.rb
+++ app/models/database.rb
@@ -1,13 +1,19 @@
 class Database
 
+  attr_accessor :exercises
   attr_accessor :measurements
 
   def initialize
+    @exercises = Array.new
     @measurements = Array.new
   end
 
   def process_enter_new_measurements(weight, body_fat, height)
     @measurements.push([weight, body_fat, height])
+  end
+  
+  def process_new_exercise(name, type, disc, muscle)
+    @exercises.push([name, type, disc, muscle])
   end
 
 end
